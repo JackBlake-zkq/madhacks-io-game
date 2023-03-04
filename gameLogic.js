a519744c87311470767b7a4391c1f41afbb38a99
Kaden Almizyed <kalmizyed@wisc.edu>
2023-03-04 15:18:14 -0600

    Implemented addPlayer() and removePlayer() Implemented player killing functionality

--- gameLogic.js
+++ gameLogic.js
@@ -1,28 +1,22 @@
 // Stores the entirety of the game's state.
 let game = {
     playerData: {
-        radius: 5
+        radius: 5,
+        startingSpeed: 5
     },
-    players: [
-        {
-            location: {
-                x: 0,
-                y: 0
-            },
-            velocity: {
-                x: 0,
-                y: 0
-            },
-            charging: false,
-            dead: false
-        }
-    ],
     map: {
         width: 100,
-        height: 100
+        height: 100,
+        spawnMargin: game.playerData.radius // The closest players can spawn to the edge of the map
     },
+    players: [],
     obstacles: []
 };
+
+const __player = {
+    charging: false,
+    dead: false
+}
 
 /*
  * The game logic that runs once per game tick.
@@ -113,7 +107,7 @@ let duel = (player, otherPlayers) => {
 }
 
 let kill = (player) => {
-    // TODO
+    player.dead = true;
 }
 
 let movePlayerCharge = (player) => {
@@ -125,10 +119,40 @@ let movePlayerStraight = (player) => {
     player.location.y += player.velocity.y;
 }
 
-let addPlayer = () => {
-    // TODO
+/*
+ * Adds a player to the player list.
+ * 
+ * name: the player name
+ * picture: the URL to the player's profile picture
+*/
+let addPlayer = (name, picture) => {
+    let newPlayer = Object.create(__player);
+
+    // Set new player info
+    newPlayer.name = name;
+    newPlayer.picture = picture;
+
+    // Set new player position
+    let spawnPadding = game.playerData.radius + game.map.spawnMargin;
+    newPlayer.position = {
+        x: (Math.random() * (game.map.width - 2*spawnPadding)) + spawnPadding,
+        y: (Math.random() * (game.map.height - 2*spawnPadding)) + spawnPadding
+    };
+
+    // Set new player velocity
+    let velNormX = Math.random();
+    let velNormY = Math.sqrt(1 - velNormX*velNormX);
+    newPlayer.velocity = {
+        x: velNormX * Math.sqrt(game.playerData.startingSpeed),
+        y: velNormY * Math.sqrt(game.playerData.startingSpeed)
+    };
+
+    game.players.push();
 }
 
-let removePlayer = () => {
-    // TODO
+/*
+ * Removes a player from the player list.
+*/
+let removePlayer = (player) => {
+    game.players = game.players.filter(currentPlayer => currentPlayer !== player);
 }
