9b7a2b4c412bc750d229e63f840570c3cf8329d9
Nathaniel Liu <nathaniel.hao.liu@gmail.com>
2023-03-04 17:56:25 -0600

    fixed get pfp and userdata from username

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.use(express.static('frontend/public'));
 
 const http = require('http');
 
+const axios = require('axios');
+
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
@@ -13,15 +15,40 @@ const io = new Server(server);
 
 io.on("connection", socket => {
     let user;
-    socket.on("login", ({ _username, password }) => {
-        // make login request
-        // if succesful:
-        //     username = _username
-        //     profilePic = get profile pic from thumbnails API
-        //     socket.emit("login-successful");
-        // otherwise:
-        //     socket.emit("login-fail");
+    socket.on("login", async input => {
+        console.log(input.username)
+        response = await axios.get
+            ("https://users.roblox.com/v1/users/search?keyword=" + input.username + "&limit=10")
+            .catch(res => {
+                switch(res.status){
+                    case 429:
+                        socket.emit("loginFail", "Too many requests");
+                        break;
+                    case 400:
+                        socket.emit("loginFail", "Username not found");
+                        break;
+                    default:
+                        socket.emit("loginFail", "Unknown error occured");
+                        break;  
+                }
+                return;
+            })
+            let userData = response.data.data[0];
+            console.log(userData)
+            res = await axios.get
+                ("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" + 
+                    userData['id'] + "&size=48x48&format=Png&isCircular=true")
+                    .then(res => {
+                        userData.pfp = res.data.data[0]['imageUrl'];
+                    })
+                    .catch(res => {
+                        console.log(res.status);
+                        userData.pfp = "https://i.imgur.com/2RnSO8r.png";
+                    }) 
+            user = userData;
+            socket.emit("loginSuccessful", user);
     });
+    
     socket.on("join-game", () => {
 
     });
