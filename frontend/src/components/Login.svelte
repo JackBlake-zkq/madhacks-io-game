52cdcf71f91b1a3416695a0dd2c3b9db31d356fc
Nathaniel Liu <nathaniel.hao.liu@gmail.com>
2023-03-04 21:31:45 -0600

    changed title screen ui alignment

--- frontend/src/components/Login.svelte
+++ frontend/src/components/Login.svelte
@@ -7,27 +7,53 @@
     }
 </script>
 
-<main>
-    <div>
-        <h1>Login with Roblox</h1>
-        <input bind:value={username} placeholder="username">
-        <input bind:value={pass} placeholder="password">
-        <button on:click={login}>Login</button>
-    </div>
+<main >
+    <h1 >(Name)</h1>
+    <p class = "center">If you want to use your roblox avatar enter your roblox username!</p>
+        <div class = "center" >
+            <input bind:value={username} placeholder="username" type = "username">
+            <!--<input bind:value={pass} placeholder="password"> -->
+            <button on:click={login}>Login</button>
+        </div>
 </main>
 
 <style>
-    main {
-        display: flex;
-        justify-content: center;
-        justify-items: center;
+    *{
+        box-sizing: border-box;
     }
     input {
         display: block;
-        width: 100%;
+        height: 3rem;
+        width: 300px;
         text-align: center;
+        background-color: var(--bg);
+        filter: brightness(3);
+        border: none;
+        border-radius: 0.5rem;
+        margin-top: 8rem;
+    }
+    input[type="username"]{
+        color:rgb(146, 146, 146);
     }
     h1 {
         text-align: center;
+        margin-top: 7rem;
+        font-size: 50px;
+    }
+    button{
+        margin:0 auto;
+        display:block;
+        margin-top: 1rem;
+        height:2rem;
+        width:40%;
+        background-color: var(--c2);
+        border: none;
+        border-radius: 0.5rem;
+        filter: brightness(.85);
+        color: rgb(255, 255, 255);
+    }
+    ::selection {
+        color: #ffb2ee;
+        background:transparent
     }
 </style>
