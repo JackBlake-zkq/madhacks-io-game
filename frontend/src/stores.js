c73517b81e7471857d7fe4ec88320a523db9cfba
Jack Blake <jhblake2@wisc.edu>
2023-03-04 21:31:24 -0600

    basic game graphics

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -6,10 +6,53 @@ export const user = writable(null, set => {
         console.log(user);
         set(user);
     });
+
+    //test user
+    set({
+        id: 0,
+        name: "player0",
+        location: {
+            x: 4200,
+            y: 3200
+        }
+    });
 });
 
 export const gameState = readable(null, set => {
     socket.on("gameTick", game => {
         set(game);
+    })
+
+    //test data
+    set({
+        playerData: {
+            radius: 10,
+            startingSpeed: 5,
+            chargeRadius: 20
+        },
+        map: {
+            width: 100,
+            height: 100,
+        },
+        players: {
+            0: {
+                id: 0,
+                name: "player0",
+                location: {
+                    x: 20,
+                    y: 30
+                },
+                pfp: "https://i.imgur.com/2RnSO8r.png"
+            },
+            1: {
+                id: 1,
+                name: "player1",
+                location: {
+                    x: 80,
+                    y: 50
+                },
+                pfp: "https://i.imgur.com/2RnSO8r.png"
+            }
+        }
     })
 });
