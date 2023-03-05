c6d694d85a6a1127e45e3665bfce0f8d57be1f8d
Nathaniel Liu <nathaniel.hao.liu@gmail.com>
2023-03-04 19:29:55 -0600

    added our unique id identifier

--- index.js
+++ index.js
@@ -12,39 +12,32 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let currID = 0;
 
 io.on("connection", socket => {
     let user;
     socket.on("login", async input => {
-        let userData;
+        let userData = {
+            name: input.username.slice(0, 20),
+            pfp: "https://i.imgur.com/2RnSO8r.png"
+        }
+        if(input.username.length < 3){
+            socket.emit("loginFailed", "Username must be at least 3 characters");
+            return;
+        }
         await axios.get
             ("https://users.roblox.com/v1/users/search?keyword=" + input.username + "&limit=10")
             .then(res => {
                 userData = res.data.data[0];
             })
-            .catch(res => {
-                switch(res.status){
-                    case 429:
-                        socket.emit("loginFail", "Too many requests");
-                        break;
-                    case 400:
-                        socket.emit("loginFail", "Username not found");
-                        break;
-                    default:
-                        socket.emit("loginFail", "Unknown error occured");
-                        break;  
-                }
-            })
-            if(!userData) return;
-            await axios.get
-                ("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" + 
-                    userData['id'] + "&size=48x48&format=Png&isCircular=true")
-                    .then(res => {
-                        userData.pfp = res.data.data[0]['imageUrl'];
-                    })
-                    .catch(res => {
-                        userData.pfp = "https://i.imgur.com/2RnSO8r.png";
-                    }) 
+            if(userData)
+                await axios.get
+                    ("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" + 
+                        userData['id'] + "&size=48x48&format=Png&isCircular=true")
+                        .then(res => {
+                            userData.pfp = res.data.data[0]['imageUrl'];
+                        })
+            userData.id = currID++;
             user = userData;
             console.log(user);
             socket.emit("loginSuccessful", user);
